4793fe7aef017f4bc601178aca7479b049d7ba7e
Roman Masyuk <romchick2003@mail.ru>
2024-09-22 22:40:32 +0300

    Create DB (remaining files)

---
+++ app/src/main/java/com/kys0main/notesapplication/repository/NoteRepository.kt
@@ -0,0 +1,12 @@
+package com.kys0main.notesapplication.repository
+
+import com.kys0main.notesapplication.database.NoteDatabase
+import com.kys0main.notesapplication.model.Note
+
+class NoteRepository(private val db : NoteDatabase) {
+    suspend fun insertNote(note : Note) = db.getNoteDao().insertNote(note)
+    suspend fun deleteNote(note : Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note : Note) = db.getNoteDao().updateNote(note)
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query : String?) = db.getNoteDao().searchNote(query)
+}
